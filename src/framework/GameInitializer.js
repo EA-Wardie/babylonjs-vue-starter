b035869aa97feedbf54c7b5cde3a9687452d1985
EA-Wardie <56725833+EA-Wardie@users.noreply.github.com>
2022-04-29 23:40:21 +0200

    player movement

--- src/framework/GameInitializer.js
+++ src/framework/GameInitializer.js
@@ -8,7 +8,7 @@ import {
     StandardMaterial,
     Texture,
     Vector3,
-    CannonJSPlugin, PhysicsImpostor,
+    CannonJSPlugin, PhysicsImpostor, DeviceSourceManager, DeviceType, KeyboardEventTypes,
 } from "@babylonjs/core";
 import '@babylonjs/loaders';
 import {MapGenerator} from 'noise-map';
@@ -19,8 +19,9 @@ export default {
         return {
             engine: null,
             scene: null,
-            camera: null,
             light: null,
+            camera: null,
+            device: null,
             ground: null,
             player: null,
         };
@@ -32,6 +33,10 @@ export default {
             this.createScene();
             this.createLight();
             this.createCamera();
+            this.createDeviceSourceManager();
+
+            //Inputs
+            this.handleInputs();
 
             // //Create game meshes
             this.createPlayer();
@@ -57,21 +62,25 @@ export default {
             this.light.intensity = 1;
         },
         createCamera() {
-            this.camera = new FreeCamera('camera', new Vector3(0, 7, -12), this.scene);
+            this.camera = new FreeCamera('camera', new Vector3(0, 15, -30), this.scene);
             this.camera.speed = 2;
             this.camera.inertia = 0.2;
             this.camera.rotation = new Vector3(0.5, 0, 0);
             this.camera.attachControl();
         },
+        createDeviceSourceManager() {
+            this.device = new DeviceSourceManager(this.scene.getEngine());
+        },
 
         //Meshes
         createPlayer() {
-            this.player = MeshBuilder.CreateBox('player', {size: 0.1, height: 1.5}, this.scene);
-            // this.player.position.y = 5;
+            this.player = MeshBuilder.CreateBox('player', {size: 1}, this.scene);
+            this.player.position.y = 10;
+            // this.player.setEnabled(false);
             // this.registerPlayerAsPhysicsImposter();
         },
         generateVoxelTerrain() {
-            this.createVoxelTerrain(10000);
+            this.createVoxelTerrain(10000, 1);
             // this.registerVoxelTerrainAsPhysicsImposter();
         },
 
@@ -79,17 +88,37 @@ export default {
         renderGame() {
             this.engine.runRenderLoop(() => {
                 this.scene.render();
+                // this.handleInputs();
+            });
+        },
+
+        //Inputs
+        handleInputs() {
+            this.handleKeyboardMovement();
+        },
+        handleKeyboardMovement() {
+            window.addEventListener('keypress', event => {
+                if (event.key === 'w') {
+                    this.player.position.z += 1;
+                } else if (event.key === 'a') {
+                    this.player.position.x -= 1;
+                } else if (event.key === 'd') {
+                    this.player.position.x += 1;
+                } else if (event.key === 's') {
+                    this.player.position.z -= 1;
+                }
             });
         },
 
         //Helpers
         createVoxelTerrain(totalVoxels, voxelSize = 0.1) {
-            const sps = new SolidParticleSystem('sps', this.scene),
+            const sps = new SolidParticleSystem('sps', this.scene, {isPickable: true}),
                 voxel = MeshBuilder.CreateBox('voxel', {size: voxelSize}, this.scene);
 
             sps.addShape(voxel, totalVoxels);
             voxel.dispose();
             this.ground = sps.buildMesh();
+
             sps.initParticles = () => {
                 let row = 0;
                 const sqrtSize = Math.sqrt(totalVoxels).toFixed(0),
@@ -99,7 +128,7 @@ export default {
                     const col = -(-p % sqrtSize),
                         particle = sps.particles[p],
                         displacement = (sqrtSize * voxelSize) / 2,
-                        particleHeight = heightMap.get(row, col);
+                        particleHeight = heightMap.get(row, col) / 0.1;
 
                     particle.position.x = (col * voxelSize) - displacement;
                     particle.position.z = (row * voxelSize) - displacement;
@@ -113,37 +142,45 @@ export default {
 
             sps.initParticles();
             sps.setParticles();
+            sps.refreshVisibleSize();
             sps.isAlwaysVisible = true;
+            sps.computeParticleTexture = false;
 
             const grassMaterial = new StandardMaterial('grass');
             grassMaterial.diffuseTexture = new Texture('./assets/textures/stone.png');
             this.ground.material = grassMaterial;
 
-            this.registerSPSEventSystem(sps);
+            // this.registerSPSEventSystem(sps);
         },
-        registerSPSEventSystem(sps) {
-            this.scene.onPointerDown = (event, result) => {
-                const id = result.faceId;
-
-                console.log(result);
-                if(id !== -1) {
-                    const picked = sps.pickedParticle(result),
-                        index = picked.idx,
-                        particle = sps.particles[index];
-
-                    console.log(picked, index, particle);
-                    this.player.position.x = particle.position.x;
-                    this.player.position.z = particle.position.z;
-                    this.player.position.y = particle.position.y;
-                }
-            };
-        },
-        registerVoxelTerrainAsPhysicsImposter() {
-            this.ground.physicsImpostor = new PhysicsImpostor(this.ground, PhysicsImpostor.BoxImpostor, {mass: 0, friction: 0.5, restitution: 1}, this.scene);
-        },
-        registerPlayerAsPhysicsImposter() {
-            this.player.physicsImpostor = new PhysicsImpostor(this.player, PhysicsImpostor.SphereImpostor, {mass: 0.1, restitution: 1}, this.scene);
-        },
+        // registerSPSEventSystem(sps, voxelSize = 1) {
+        //     this.scene.onPointerDown = (_, pickResult) => {
+        //         const faceId = pickResult.faceId;
+        //         if (faceId === -1) {
+        //             return;
+        //         }
+        //         const picked = sps.pickedParticle(pickResult),
+        //             idx = picked.idx,
+        //             particle = sps.particles[idx];
+        //
+        //         this.player.setEnabled(true);
+        //         this.player.position.x = particle.position.x;
+        //         this.player.position.z = particle.position.z;
+        //         this.player.position.y = particle.position.y + 1;
+        //     };
+        // },
+        // registerVoxelTerrainAsPhysicsImposter() {
+        //     this.ground.physicsImpostor = new PhysicsImpostor(this.ground, PhysicsImpostor.BoxImpostor, {
+        //         mass: 0,
+        //         friction: 0.5,
+        //         restitution: 1
+        //     }, this.scene);
+        // },
+        // registerPlayerAsPhysicsImposter() {
+        //     this.player.physicsImpostor = new PhysicsImpostor(this.player, PhysicsImpostor.SphereImpostor, {
+        //         mass: 0.1,
+        //         restitution: 1
+        //     }, this.scene);
+        // },
         getPerlinNoiseHeightMap(width, height) {
             const generator = new MapGenerator(Math.random(), {type: 'perlin'});
             return generator.createMap(width, height);
